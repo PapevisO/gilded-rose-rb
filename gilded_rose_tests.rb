be44fc03c2da6ccc90b08f9509c0514385f05473
papevis_o <sasha@papevis.com>
2019-12-16 03:39:08 +0200

    Update gilded_rose_tests.rb

--- gilded_rose_tests.rb
+++ gilded_rose_tests.rb
@@ -5,6 +5,11 @@ class TestUntitled < Test::Unit::TestCase
   def test_foo
     items = [Item.new('foo', 0, 0)]
     GildedRose.new(items).update_quality()
-    assert_equal items[0].name, 'fixme'
+    assert_equal items[0].name, 'foo'
+  end
+  def test_avalance_expirable
+    items = [Item.new('B', 0, 10)]
+    GildedRose.new(items).update_quality()
+    assert_equal items[0].quality, 0
   end
 end
