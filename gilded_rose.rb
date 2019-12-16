685507b5ca402dd97178ce5d851d489915354ff5
Oleksandr Papevis <sasha@papevis.com>
2019-12-16 15:30:13 +0200

    almost there

--- gilded_rose.rb
+++ gilded_rose.rb
@@ -1,24 +1,96 @@
 class GildedRose
-  @kinds = {
-      expirable_avalance: {
-          expirable: [
-              {type: :avalance}
-          ],
-          excitable: [
-              {type: :sustain, less_than: 11, setpoint: 50},
-              {type: :sustain, less_than: 5, setpoint: 50}
-          ],
-          upgradeable: [
-              {type: :sustain, setpoint: 50}
-          ]
-      }
-  }
+  PARAM_TYPES = { sustain: 100, avalance: 200, gradual: 300, default: 400 }.freeze
+
+  def item_process_by_cat(item, category)
+    Array(category[:upgradeable]).each do |upgradeable|
+      if upgradeable[:type] === :sustain
+        if upgradeable.fetch(:rate, 1) < 0
+          if item.quality > upgradeable.fetch(:setpoint, 50)
+            item.quality += upgradeable.fetch(:rate, 1)
+          end
+        else
+          if item.quality < upgradeable.fetch(:setpoint, 50)
+            item.quality += upgradeable.fetch(:rate, 1)
+          end
+        end
+      else
+        item.quality += upgradeable.fetch(:rate, 1)
+      end
+    end
+    Array(category[:excitable]).each do |excitable|
+      next if (
+        (!excitable[:sell_in_is_greater_than].nil?) &&
+        (item.sell_in <= excitable[:sell_in_is_greater_than])
+      )
+      next if (
+        (!excitable[:sell_in_is_less_than].nil?) &&
+        (item.sell_in >= excitable[:sell_in_is_less_than])
+      )
+
+      if excitable[:type] === :sustain
+        if item.quality < excitable.fetch(:setpoint, 50)
+          item.quality += excitable.fetch(:rate, 1)
+        end
+      else
+        item.quality += excitable.fetch(:rate, 1)
+      end
+    end
+    item.sell_in -= 1 unless Array(category[:expirable]).count.zero?
+    Array(category[:expirable]).each do |expirable|
+      if expirable[:type] === :sustain
+        if item.quality > expirable.fetch(:setpoint, 0) and item.sell_in < 0
+          item.quality -= expirable.fetch(:rate, 1)
+        end
+      elsif expirable[:type] === :avalance
+        if item.quality > expirable.fetch(:setpoint, 0) and item.sell_in < 0
+          item.quality = 0
+          return
+        end
+      elsif expirable[:type] === :gradual
+        item.quality -= expirable.fetch(:rate, 1)
+      else
+        item.quality -= expirable.fetch(:rate, 1)
+      end
+    end
+  end
 
   def initialize(items)
+    @kinds = {
+      expirable_avalance_excitable_sustain_upgradable: {
+        upgradeable: [
+            {type: :sustain, setpoint: 50}],
+        excitable: [
+            {type: :sustain, sell_in_is_less_than: 11, setpoint: 50},
+            {type: :sustain, sell_in_is_less_than: 6, setpoint: 50}],
+        expirable: [
+            {type: :avalance}
+        ]},
+      expirable_sustain_upgradable_sustain: {
+        upgradeable: [
+            {type: :sustain, setpoint: 50}],
+        expirable: [
+            {type: :sustain, setpoint: 50},
+        ]},
+      default: {
+        upgradeable: [
+            {type: :sustain, setpoint: 0, rate: -1}
+        ],
+        expirable: [
+            {type: :sustain, setpoint: 0}
+        ]}
+    }
+
+    @item_categories = {
+        'Aged Brie' => @kinds[:expirable_sustain_upgradable_sustain],
+        'Backstage passes to a TAFKAL80ETC concert' => @kinds[:expirable_avalance_excitable_sustain_upgradable],
+        '' => @kinds[:default],
+        :default => @kinds[:default]
+    }
+
     @items = items
   end
 
-  def update_quality()
+  def update_quality
     @items.each do |item|
       if item.name != 'Aged Brie' and item.name != 'Backstage passes to a TAFKAL80ETC concert'
         if item.quality > 0
@@ -65,6 +137,12 @@ class GildedRose
       end
     end
   end
+
+  def update_quality_draft
+    @items.each do |item|
+      item_process_by_cat(item, @item_categories.fetch(item.name, @kinds[:default]))
+    end
+  end
 end
 
 class Item
@@ -76,7 +154,7 @@ class Item
     @quality = quality
   end
 
-  def to_s()
+  def to_s
     "#{@name}, #{@sell_in}, #{@quality}"
   end
 end
--- gilded_rose_tests.rb
+++ gilded_rose_tests.rb
@@ -4,80 +4,83 @@ require 'test/unit'
 class TestUntitled < Test::Unit::TestCase
   def test_foo
     items = [Item.new('foo', 0, 0)]
-    GildedRose.new(items).update_quality()
+    GildedRose.new(items).update_quality
     assert_equal items[0].name, 'foo'
   end
-  def test_expirable_avalance
-    items = [Item.new('Backstage passes to a TAFKAL80ETC concert', 0, 10)]
-    GildedRose.new(items).update_quality()
+  # test expirable avalance  excitable sustain  upgradable
+  def test_expava_excsus_upgradable
+    b_name = 'Backstage passes to a TAFKAL80ETC concert'
+    items = [Item.new(b_name, 0, 10)]
+    GildedRose.new(items).update_quality_draft
     assert_equal items[0].quality, 0
     assert_equal items[0].sell_in, -1
-    GildedRose.new(items).update_quality()
+    GildedRose.new(items).update_quality_draft
     assert_equal items[0].quality, 0
     assert_equal items[0].sell_in, -2
-  end
-  def test_expirable_avalance_excitable_sustain_upgradable
-    b_name = 'Backstage passes to a TAFKAL80ETC concert'
+
     items = [Item.new(b_name, 13, 20),
              Item.new(b_name, 8,  20),
              Item.new(b_name, 5,  20)]
-    GildedRose.new(items).update_quality()
+    GildedRose.new(items).update_quality_draft
     assert_equal items[0].to_s, b_name + ", 12, 21"
     assert_equal items[1].to_s, b_name +  ", 7, 22"
     assert_equal items[2].to_s, b_name +  ", 4, 23"
-    GildedRose.new(items).update_quality()
+    GildedRose.new(items).update_quality_draft
     assert_equal items[0].to_s, b_name + ", 11, 22"
     assert_equal items[1].to_s, b_name +  ", 6, 24"
     assert_equal items[2].to_s, b_name +  ", 3, 26"
-    GildedRose.new(items).update_quality()
+    GildedRose.new(items).update_quality_draft
     assert_equal items[0].to_s, b_name + ", 10, 23"
     assert_equal items[1].to_s, b_name +  ", 5, 26"
     assert_equal items[2].to_s, b_name +  ", 2, 29"
-    GildedRose.new(items).update_quality()
+    GildedRose.new(items).update_quality_draft
     assert_equal items[0].to_s, b_name +  ", 9, 25"
     assert_equal items[1].to_s, b_name +  ", 4, 29"
     assert_equal items[2].to_s, b_name +  ", 1, 32"
-    GildedRose.new(items).update_quality()
-    GildedRose.new(items).update_quality()
+    GildedRose.new(items).update_quality_draft
+    GildedRose.new(items).update_quality_draft
     assert_equal items[0].to_s, b_name +  ", 7, 29"
     assert_equal items[1].to_s, b_name +  ", 2, 35"
     assert_equal items[2].to_s, b_name + ", -1, 0"
-    GildedRose.new(items).update_quality()
-    GildedRose.new(items).update_quality()
+    GildedRose.new(items).update_quality_draft
+    GildedRose.new(items).update_quality_draft
     assert_equal items[0].to_s, b_name +  ", 5, 33"
     assert_equal items[1].to_s, b_name +  ", 0, 41"
     assert_equal items[2].to_s, b_name + ", -3, 0"
-    GildedRose.new(items).update_quality()
+    GildedRose.new(items).update_quality_draft
     assert_equal items[0].to_s, b_name +  ", 4, 36"
     assert_equal items[1].to_s, b_name + ", -1, 0"
     assert_equal items[2].to_s, b_name + ", -4, 0"
   end
-  def test_expirable_gradual_degradable
+  # test  expirable gradual  degradable
+  def test_expgrad_degrad
     items = [Item.new('Helium balloon', 0, 52),Item.new('Helium balloon', 2, 48)]
-    GildedRose.new(items).update_quality()
+    GildedRose.new(items).update_quality_draft
     assert_equal items[0].quality, 50
     assert_equal items[0].sell_in, -1
     assert_equal items[1].quality, 47
     assert_equal items[1].sell_in, 1
-    GildedRose.new(items).update_quality()
+    GildedRose.new(items).update_quality_draft
     assert_equal items[0].quality, 48
     assert_equal items[0].sell_in, -2
     assert_equal items[1].quality, 46
     assert_equal items[1].sell_in, 0
-    GildedRose.new(items).update_quality()
+    GildedRose.new(items).update_quality_draft
     assert_equal items[0].quality, 46
     assert_equal items[0].sell_in, -3
     assert_equal items[1].quality, 44
     assert_equal items[1].sell_in, -1
-    GildedRose.new(items).update_quality()
+    GildedRose.new(items).update_quality_draft
     assert_equal items[0].quality, 44
     assert_equal items[0].sell_in, -4
     assert_equal items[1].quality, 42
     assert_equal items[1].sell_in, -2
   end
-  def test_expirable_sustain_upgradable_sustain
-    items = [Item.new('Aged Brie', 0, 52),Item.new('Aged Brie', 2, 48)]
-    GildedRose.new(items).update_quality()
+  # test  expirable sustain  upgradable sustain
+  def test_expsus_upgrsus
+    items = [Item.new('Aged Brie', 0, 52),
+             Item.new('Aged Brie', 2, 48)]
+    GildedRose.new(items).update_quality_draft
     assert_equal items[0].quality, 52
     assert_equal items[0].sell_in, -1
     assert_equal items[1].quality, 49
@@ -91,7 +94,7 @@ class TestUntitled < Test::Unit::TestCase
     assert_equal items[1].quality, 50
     assert_equal items[1].sell_in, -1
   end
-  def test_expirable_avalance
+  def test_stable
     c_name = 'Sulfuras, Hand of Ragnaros'
     items = [Item.new(c_name, 0, 10),
              Item.new(c_name, -1, 51),
